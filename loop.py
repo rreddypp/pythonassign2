054ab35b962677e4703b1b0e718f7fa2cd48dfff
rreddypp <rajreshrr77@gmail.com>
2025-03-07 22:35:47 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,10 @@
+# Initialize the sum variable
+total_sum = 0
+
+# Iterate over numbers from 1 to 50 and add them to total_sum
+for num in range(1, 51):
+    total_sum += num
+
+# Display the final sum
+print("The sum of numbers from 1 to 50 is:", total_sum)
+print("Thank you")

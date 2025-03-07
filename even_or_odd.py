a61c725e53b69b6973079a1732f0ee0e79d37e2a
rreddypp <rajreshrr77@gmail.com>
2025-03-07 22:35:14 +0530

    Add files via upload

---
+++ even_or_odd.py
@@ -0,0 +1,10 @@
+# Take an integer input from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is an Even number.")
+else:
+    print(f"{num} is an Odd number.")
+
+print("Thank you")
